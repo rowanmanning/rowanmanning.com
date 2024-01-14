78ba5d8fdd4af1456158d217cd6cd575b80f8a0a
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2024-01-14 23:56:40 +0000

    Install the correct Hugo version

---
+++ scripts/download-hugo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.101.0/hugo_0.101.0_macOS-64bit.tar.gz --output node_modules/.bin/hugo.tar.gz
+cd node_modules/.bin
+gunzip -c hugo.tar.gz | tar xopf -
+rm hugo.tar.gz
+cd -
