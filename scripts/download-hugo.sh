7fe07a90461ff21481e965aa1bb6e52f1fbfc91e
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2024-05-26 22:45:58 +0100

    Install the extended version of Hugo

--- scripts/download-hugo.sh
+++ scripts/download-hugo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.101.0/hugo_0.101.0_macOS-64bit.tar.gz --output node_modules/.bin/hugo.tar.gz
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.101.0/hugo_extended_0.101.0_macOS-64bit.tar.gz  --output node_modules/.bin/hugo.tar.gz
 cd node_modules/.bin
 gunzip -c hugo.tar.gz | tar xopf -
 rm hugo.tar.gz
